3c53c6d2bf0744ddfa036620ecbb8c47a9a77133
MarkPushkar92 <mark.pushkar92@gmail.com>
2022-09-13 21:33:00 +1000

    images added to cells

--- MYLIST/ViewController.swift
+++ MYLIST/ViewController.swift
@@ -8,12 +8,75 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: PROPERTIES
+   
+    private let mockArray = [
+        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+        "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+        "Speak Easy", "Morris Pub", "Вкусные истории",
+        "Классик", "Love&Life", "Шок", "Бочка"
+    ]
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.backgroundColor = .white
+        tableView.toAutoLayout()
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(TableViewCell.self, forCellReuseIdentifier: cellID)
+        return tableView
+    }()
+    
+    private let cellID = "cellID"
+   
+    
+    //MARK: FUNCS
+    
+   
+    
+    //MARK: LIFECYCLE
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupViews()
+        
     }
+}
 
+//MARK: EXTENSIONS
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mockArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as! TableViewCell
+        cell.backgroundColor = .cyan
+        cell.textLabel?.text = mockArray[indexPath.row]
+        cell.imageView?.image = UIImage(named: mockArray[indexPath.row])
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    
+}
+
+private extension ViewController {
+        func setupViews() {
+        title = "My List"
+        view.addSubview(tableView)
+        view.backgroundColor = .systemBackground
+        let constraints = [
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ]
+        NSLayoutConstraint.activate(constraints)
+    }
 }
 
