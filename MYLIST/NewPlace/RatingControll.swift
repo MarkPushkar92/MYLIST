af784604dbb8399c5cb7c6cc081e5ac76d81e338
MarkPushkar92 <mark.pushkar92@gmail.com>
2022-12-02 21:46:47 +0700

    rating in progress , stars applied

--- MYLIST/NewPlace/RatingControll.swift
+++ MYLIST/NewPlace/RatingControll.swift
@@ -11,15 +11,22 @@ class RatingControll: UIStackView {
     
     // MARK: Propreties
     
+   // private var starSize: CGSize = CGSize(width: 44, height: 44)
+    
+    private var starCount: Int = 5
+    
     private var ratingButtons = [UIButton]()
     
-    var rating = 0
+    var rating = 0 {
+        didSet {
+            updateButtonSelectionStates()
+        }
+    }
 
     // MARK: Init
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .blue
         setupButtons()
     }
     
@@ -31,25 +38,41 @@ class RatingControll: UIStackView {
     // MARK: Private methods
     
     private func setupButtons() {
-        for _ in 1 ... 5 {
+        
+        let filledStar = UIImage(named: "filledStar")
+        let emptyStar = UIImage(named: "emptyStar")
+        let highlightedStar = UIImage(named: "highlightedStar")
+        
+        for _ in 1 ... starCount {
             let button = UIButton()
             button.addTarget(self, action: #selector(ratingButtonTapped), for: .touchUpInside)
             button.toAutoLayout()
-            button.backgroundColor = .red
-            addArrangedSubview(button)
-//            let constraints = [
-//                button.heightAnchor.constraint(equalToConstant: 44),
-//                button.widthAnchor.constraint(equalToConstant: 44),
-//            ]
-//            NSLayoutConstraint.activate(constraints)
+            button.setImage(emptyStar, for: .normal)
+            button.setImage(filledStar, for: .selected)
+            button.setImage(highlightedStar, for: .highlighted)
+            button.setImage(highlightedStar, for: [.highlighted, .selected])
+            addArrangedSubview(button)
             ratingButtons.append(button)
+        }
+      //  updateButtonSelectionStates()
+        print(ratingButtons.count)
+    }
+    
+    @objc func ratingButtonTapped(button: UIButton) {
+        print("butttonnn presssed")
+        
+        guard let index = ratingButtons.firstIndex(of: button) else  { return }
+        let selectedRating = index + 1
+        if selectedRating == rating {
+            rating = 0
+        } else {
+            rating = selectedRating
         }
     }
     
-    @objc func ratingButtonTapped() {
-        print("butttonnn presssed")
+    private func updateButtonSelectionStates() {
+        for (index, button) in ratingButtons.enumerated() {
+            button.isSelected = index < rating
+        }
     }
-    
-    
-    
 }
